649da38d6784fdbbec1501acd161b33116ee6442
Bunyod1001 <126106481+Bunyod1001@users.noreply.github.com>
2023-02-25 15:43:50 +0500

    Add files via upload

---
+++ js/1dars.js
@@ -0,0 +1,25 @@
+
+"use strict";
+let ism = prompt("Ismingiz nima?")
+console.log("Ism:" + ism);
+
+let familya = prompt("Familyangiz nima?")
+console.log("Familiya" + familya);
+
+let yosh = prompt("Yoshingiz nechida?")
+console.log("Yosh:" + yosh);
+
+let locations = prompt("Tug'ilgan joyingiz")
+console.log("Manzil:" + locations);
+
+let Millat = prompt("Millatingiz nima?")
+console.log("Millat:" + Millat);
+
+let OtangizIsmi = prompt("Otangizning ismi")
+console.log("Otasining ismi:" + OtangizIsmi);
+
+let OnangizIsmi = prompt("Onangizning ismi")
+console.log("Onasining ismi:" + OnangizIsmi);
+
+alert("True");
+console.log("Tog'rimi:");
